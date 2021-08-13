e21a8cae9a670ff821f396e53facf7f91cfd9ad0
tt <2261923297@qq.com>
2021-08-13 03:38:53 -0400

    client server

--- SuperFile/SuperFile/client.cc
+++ SuperFile/SuperFile/client.cc
@@ -6,11 +6,8 @@
 int test_trans_file(const std::string& filePath, std::string& ip) {
 	int ans = 0;
 	SuperFile::ptr sf = SuperFile::ptr(new SuperFile);
-<<<<<<< HEAD
-	if( -1 == sf->searchServer("192.168.60.24", 6666)) {
-=======
+
 	if( -1 == sf->searchServer(ip.c_str(), PORT)) {           //connect
->>>>>>> 461766edd0db2a4732eb99f24cd32a533b3ac0bd
 		std::cout << "cant search server!" << std::endl;
 		return -1;
 	}
@@ -19,11 +16,8 @@ int test_trans_file(const std::string& filePath, std::string& ip) {
 	int i = 1;
 	while(i--) {
 		recv_file = "...";
-<<<<<<< HEAD
-		sf->recvFile(0, "D:\\SuperFile\\recv.iso");
-=======
+
 		sf->recvFile(sf->getSock(), filePath);                     //recv
->>>>>>> 461766edd0db2a4732eb99f24cd32a533b3ac0bd
 		std::cout << "recvFile: " << recv_file << std::endl;
 	
 		for(int i = 0; i < 100000; i++) {
@@ -38,10 +32,10 @@ int test_trans_file(const std::string& filePath, std::string& ip) {
 int main(int argc, char** argv) {
 	std::string file_path = "D:\\SuperFile\\recv.exe";
 	std::string ip = "127.0.0.1";
-	if(argc > 2) {
+	if(argc >= 2) {
 		file_path = argv[1];
 	}
-	if(argc > 3) {
+	if(argc >= 3) {
 		ip = argv[2];
 	}
 	test_trans_file(file_path, ip);
