1c02987be651a929408019a9976c8f5f5b9463d1
tt <2261923297@qq.com>
2021-08-12 20:02:31 -0400

    compal 8/12

--- SuperFile/SuperFile/client.cc
+++ SuperFile/SuperFile/client.cc
@@ -4,7 +4,7 @@
 int test_trans_file() {
 	int ans = 0;
 	SuperFile::ptr sf = SuperFile::ptr(new SuperFile);
-	if( -1 == sf->searchServer("127.0.0.1", 6666)) {
+	if( -1 == sf->searchServer("192.168.60.24", 6666)) {
 		std::cout << "cant search server!" << std::endl;
 		return -1;
 	}
@@ -13,7 +13,7 @@ int test_trans_file() {
 	int i = 1;
 	while(i--) {
 		recv_file = "...";
-		sf->recvFile(0, "D:\\SuperFile\\recv.exe");
+		sf->recvFile(0, "D:\\SuperFile\\recv.iso");
 		std::cout << "recvFile: " << recv_file << std::endl;
 	
 		for(int i = 0; i < 100000; i++) {
